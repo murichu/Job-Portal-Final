4cff94c398e0ff61895ab05dc4ef92a384a8e498
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2026-05-04 20:35:23 +0300

    Add onboarding route

---
+++ server/routes/onboardingRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import Company from "../models/Company.js";
+import User from "../models/User.js";
+import TenantSubscription from "../models/TenantSubscription.js";
+import bcrypt from "bcrypt";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { name, email, password, companyName } = req.body;
+
+  const company = await Company.create({ name: companyName });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+    role: "admin",
+    tenantId: company._id,
+  });
+
+  await TenantSubscription.create({
+    tenantId: company._id,
+    monthlyAmount: 1000,
+    currentPeriodEnd: new Date(Date.now() + 30*24*60*60*1000),
+    nextInvoiceAt: new Date(Date.now() + 30*24*60*60*1000),
+  });
+
+  res.json({ success: true, user });
+});
+
+export default router;
